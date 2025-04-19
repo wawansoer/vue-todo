7533e1bf5daad566831daa185ee4a77d80d07f02
Sutisna Utamasjida Septiawan <wawansoer@gmail.com>
2025-04-19 10:27:51 +0700

    refactor: simplify app initialization
    - Removed unused router setup.
    - Added Pinia store for state management.
    - Streamlined app creation process.

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,14 @@
 import "@/assets/main.css"
 
 import { createApp } from "vue"
-import { createRouter, createWebHistory } from "vue-router"
-import uiPlugin from "@nuxt/ui/vue-plugin"
 
+import uiPlugin from "@nuxt/ui/vue-plugin"
+import { createPinia } from "pinia";
 import App from "@/App.vue"
 
 const app = createApp(App)
 
-const router = createRouter({
-  routes: [],
-  history: createWebHistory(),
-})
-
 app.use(uiPlugin)
-app.use(router)
+app.use(createPinia());
 
 app.mount("#app")
