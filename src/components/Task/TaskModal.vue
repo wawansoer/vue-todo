19eb146f885fc8f9bd912eda902b6bbf46062ea2
Sutisna Utamasjida Septiawan <wawansoer@gmail.com>
2025-04-19 10:29:51 +0700

    feat(task): add task modal component
    - Created TaskModal component
    - Uses UModal for display
    - Includes TaskForm component
    - Handles modal opening/closing
    - Dynamic header text based on type

---
+++ src/components/Task/TaskModal.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+import { useModalStore } from "@/stores/modalStore";
+import { modalType } from "@/types/modalType";
+import { computed, ref } from "vue";
+import TaskForm from "@/components/Task/TaskForm.vue";
+
+const props = defineProps<{
+  modalName: modalType;
+}>();
+
+const modalStore = useModalStore();
+
+const open = ref(true);
+
+const headerText = computed(() => {
+  switch (props.modalName) {
+    case modalType.ADD_TASK:
+      return "Fill this form to add your task";
+    default:
+      return "";
+  }
+});
+
+const closeModal = () => {
+  modalStore.close(props.modalName);
+  open.value = false;
+};
+</script>
+
+<template>
+  <UModal
+    v-model:open="open"
+    direction="bottom"
+    :title="props.modalName"
+    :description="headerText"
+    :dismissible="false"
+    :close="{ onClick: () => closeModal() }"
+  >
+    <template #body>
+      <div>
+        <TaskForm v-if="[modalType.ADD_TASK].includes(props.modalName)" />
+      </div>
+    </template>
+  </UModal>
+</template>
